f5df8801a059969c75579d390fb501c48d7c903d
tam.doan <tam.doan@codecomplete.jp>
2020-12-01 16:21:23 +0700

    add images to redux

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,3 +1,7 @@
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import authReducer from '../reducers/auth';
 
 export const userLoginFetch = user => {
